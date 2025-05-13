440b36335c607bc0acd777161989defa1977b9c2
02-ow <2671433907@qq.com>
2025-05-13 09:34:55 +0800

    Add files via upload

---
+++ register.js
@@ -0,0 +1,9 @@
+// 判断用户名是否符合要求
+const username = document.querySelector('.username');
+const usernameError = document.querySelector('.error-message');
+
+username.addEventListener('change',function(){
+// 定义正则表达式
+const re = /^[a-zA-Z]$/;
+
+})
